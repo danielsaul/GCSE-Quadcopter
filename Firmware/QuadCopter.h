eb5e32fdb3332d5fcfb4546785dee9beb2c4e65a
Daniel Saul <daniel@pleione.dansaul.co.uk>
2011-12-30 18:24:40 +0000

    Updated Repo

---
+++ Firmware/QuadCopter.h
@@ -0,0 +1,13 @@
+/*
+  QuadCopter - GCSE Electronic Products
+  Daniel Saul - www.DanSaul.co.uk
+  (C) Copyright 2011. All Rights Reserved.
+ 
+  Arduino-based Quadcopter Control System
+ 
+  #File     : QuadCopter.h
+  #Author   : Daniel Saul
+
+ */
+
+#define BAUD_RATE       115200
---
+++ Firmware/QuadCopter.pde
@@ -0,0 +1,16 @@
+/*
+  QuadCopter - GCSE Electronic Products
+  Daniel Saul - www.DanSaul.co.uk
+  (C) Copyright 2011. All Rights Reserved.
+ 
+  Arduino-based Quadcopter Control System
+ 
+  #File     : QuadCopter.pde
+  #Author   : Daniel Saul
+
+ */
+
+void setup(){
+
+
+}
